23c96ed84fd952bdeb53a5e3de380b027017ec01
veeru153 <32760644+veeru153@users.noreply.github.com>
2022-09-07 01:27:03 +0530

    Fixed scope issue

--- src/cmds/_Command.ts
+++ src/cmds/_Command.ts
@@ -21,9 +21,13 @@ class Command {
     run = async (client: Client, message: Message, args: string[]) => {}
 
     _canUserInvokeCmd = (member: GuildMember) => {
-        if (this.scope == undefined || this.scope == null || this.scope.length == 0) return true;
-        for (let role in this.scope) {
-            if (!member.roles.cache.has(role)) return false;
+        if (this.scope == undefined || this.scope == null || this.scope.length == 0)
+            throw new Error("Scope is null or empty.")
+
+        for (let role of this.scope) {
+            if (!member.roles.cache.has(role)) {
+                throw new Error("Insufficient Permissions.");
+            }
         }
 
         return true;
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ const client = new Client({ intents: intents, partials: partials });
 
 client.on("ready", () => {
     const env = process.env.NODE_ENV == "dev" ? "Development" : "Production";
-    const identity = process.env.NODE_ENV == "dev" ? "Cookie Dough" : "Cookie Bot";
+    const identity = process.env.NODE_ENV == "dev" ? "Cookie Dough" : "Cookie";
     console.log(`READY! Logged in as ${identity}.`);
     console.log(`- Environment: ${env}`);
-    console.log(Object.entries(cmds))
+    // console.log(Object.entries(cmds))
 })
 
 client.on("error", console.log);
+client.on("debug", console.log);
 client.on("messageCreate", async (message) => { messageCreate(client, message) });
 
 const TOKEN = process.env.NODE_ENV == "dev" ? process.env.DEV_TOKEN : process.env.TOKEN;
