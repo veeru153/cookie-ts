3937574e27edaead5d2f4403783b8c9d973817fa
veeru153 <32760644+veeru153@users.noreply.github.com>
2022-09-26 22:19:37 +0530

    created repo with maps

--- src/util/collections_v2.ts
+++ src/util/collections_v2.ts
@@ -37,8 +37,48 @@ class Repository {
     }
 }
 
+
+class RepoDocument {
+    name: string;
+    collection: FirebaseFirestore.CollectionReference;
+    doc: FirebaseFirestore.DocumentReference;
+    data: Map<string, FirebaseFirestore.DocumentData>;
+
+    constructor(collectionName: string, docName: string) {
+        this.name = `${collectionName}/${docName}`;
+        this.collection = db.collection(collectionName);
+        this.doc = this.collection.doc(docName);
+        this.data = new Map();
+
+        this.doc.onSnapshot(snapshot => {
+            const data = snapshot.data();
+            Object.entries(data).forEach(d => {
+                this.data.set(d[0], d[1]);
+            })
+        })
+    }
+
+    initialize = async () => {
+        logger.info(`[Collection_${this.name}] Initialized`);
+    }
+
+    get = (key: string) => {
+        return this.data.get(key);
+    }
+
+    set = async (key: string, value: FirebaseFirestore.DocumentData) => {
+        if(!this.data.has(key)) {
+            await this.collection.doc(key).set(value);
+            return;
+        }
+
+        await this.collection.doc(key).update(value);
+    }
+
+}
+
 export const ranksRepo = new Repository("ranks");
 export const inventoryRepo = new Repository("inventory");
-export const eventRepo = new Repository("events");
+export const eventsRepo = new Repository("events");
 
 // export default Repository;
