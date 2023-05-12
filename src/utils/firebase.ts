01ecca887e3d31fed0a63af68ca270db2b17a0b3
veeru153 <32760644+veeru153@users.noreply.github.com>
2023-05-12 17:31:44 +0530

    yes im debugging on prod

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,7 +4,7 @@ const serviceAccount = {
     "type": "service_account",
     "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "client_id": process.env.FIREBASE_CLIENT_ID,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -13,7 +13,7 @@ const serviceAccount = {
     "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
 }
 
-console.log(serviceAccount);
+console.log("service account", serviceAccount);
 
 admin.initializeApp({
     credential: admin.credential.cert((serviceAccount as ServiceAccount))
